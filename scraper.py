c138719fadc8c80407ddda6ed7b2b313cadb66ce
Ray Assch <reinier_a@hotmail.com>
2017-07-18 12:44:12 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -17,7 +17,7 @@ def scrape_table(root):
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
-            scraperwiki.datastore.save(["Artist"], record)
+            scraperwiki.sqlite.save(["Artist"], record)
         
 # scrape_and_look_for_next_link function: calls the scrape_table
 # function, then hunts for a 'next' link: if one is found, calls itself again
