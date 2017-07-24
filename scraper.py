f0ce4da1cf8c5e069f5e234bb2a9d9283ad5084a
Ray Assch <reinier_a@hotmail.com>
2017-07-24 16:07:35 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -15,9 +15,7 @@ def scrape_table(root):
             record['Region'] = table_cells[2].text_content()
             record['Trolley total'] = table_cells[3].text_content()
             record['Sales m'] = table_cells[4].text_content()
-            table_cellsurls = table_cells[1].cssselect("a")
-            record['HospitalURL'] = table_cellsurls[1].attrib.get('href')
-
+            
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
